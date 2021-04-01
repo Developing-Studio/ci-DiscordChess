70ed34cfc59673a36ddd4f5adc353bea9e8e4b29
Noxmain <x>
2021-04-01 20:17:07 +0200

    Edited chess.py

--- Chess/test_chess.py
+++ Chess/test_chess.py
@@ -1,4 +1,17 @@
 from Chess.chess import *
 
+letters = "pbnrqkPBNRQK-"
+symbols = "♟♝♞♜♛♚♙♗♘♖♕♔ "
+
+
+def print_fen(fen):
+    for line in fen.split(" ")[0].split("/"):
+        for item in numbers_to_dashes(line):
+            print(symbols[letters.index(item)] + " ", end="")
+        print()
+
+
 g = ChessGame()
-print(g.get_position("g4"))
+print(g.get_position("e1"))
+print_fen(g.game)
+print_fen("r1bqkb1r/pp2pppp/2p2n2/2nP4/2P1B3/8/PP1P1PPP/RNBQK1NR w KQkq - 5 6")
