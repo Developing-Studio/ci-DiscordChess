030337cc662b73585b3d766e7dedad8ab9a9924b
cephox <paul-stier@gmx.de>
2021-04-02 18:47:05 +0200

    Added emoji representation

--- DiscordBot/utils.py
+++ DiscordBot/utils.py
@@ -1,21 +1,37 @@
 from discord.ext.commands import AutoShardedBot
 
 
-def figure_to_emoji(figure: str):
-    return {
-        "R": "<:rook_white:827233035971461230>",
-        "r": "<:rook_black:827233035815747595>",
-        "Q": "<:queen_white:827233036004360283>",
-        "q": "<:queen_black:827233035920998441>",
-        "P": "<:pawn_white:827233034251272262>",
-        "p": "<:pawn_black:827233033874702357>",
-        "N": "<:knight_white:827233034251010098>",
-        "n": "<:knight_black:827233027117154425>",
-        "K": "<:king_white:827233027020161044>",
-        "k": "<:king_black:827233027058827345>",
-        "B": "<:bishop_white:827233025145831474>",
-        "b": "<:bishop_black:827233024730726421>"
-    }[figure]
+def figure_to_emoji(figure: str, field_number: int):
+    emojis: dict = {
+        "Ql": "<:lqueenw:827569943955505153>",
+        "kl": "<:lkingb:827569943972544523>",
+        "qd": "<:dqueenb:827569943985127436>",
+        "nl": "<:lknightb:827569944057217085>",
+        "Pl": "<:lpawnw:827569944169676901>",
+        "ql": "<:lqueenb:827569944178589716>",
+        "Kl": "<:lkingw:827569944203624498>",
+        "Qd": "<:dqueenw:827569944228135003>",
+        "kd": "<:dkingb:827569944245829633>",
+        "Rl": "<:lrookw:827569944270602271>",
+        "rl": "<:lrookb:827569944278597632>",
+        "Kd": "<:dkingw:827569944279121951>",
+        "bd": "<:dbishopb:827569944304156682>",
+        "Pd": "<:dpawnw:827569944312152075>",
+        "bl": "<:lbishopb:827569944328667156>",
+        "rd": "<:drookb:827569944333254696>",
+        "pl": "<:lpawnb:827569944358944788>",
+        "nd": "<:dknightb:827569944401149972>",
+        "Bd": "<:dbishopw:827569944404819978>",
+        "Nl": "<:lknightw:827569944438505482>",
+        "Bl": "<:lbishopw:827569944442830919>",
+        "pd": "<:dpawnb:827569944472453150>",
+        "-l": "<:tile_light:827569944501288970>",
+        "Rd": "<:drookw:827569944517541919>",
+        "Nd": "<:dknightw:827569944681775144>",
+        "-d": "<:tile_dark:827569944991367228>",
+    }
+    light_dark = "d" if not field_number else "l"
+    return emojis[figure + light_dark]
 
 
 def add_cogs(bot: AutoShardedBot, cogs):
