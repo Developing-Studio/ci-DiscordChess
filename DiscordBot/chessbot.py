0dd4f5ebc8b6a0d9d10c3b1aa5defa9af51279b9
cephox <paul-stier@gmx.de>
2021-04-03 11:12:28 +0200

    Removed dynamic emoji retrieval

--- DiscordBot/chessbot.py
+++ DiscordBot/chessbot.py
@@ -11,15 +11,6 @@ bot = AutoShardedBot(command_prefix="%")
 @bot.event
 async def on_ready():
     print("Logged in as " + bot.user.name)
-    print()
-    for i in bot.guilds[0].emojis:
-        ids = "<:{name}:{id}>".format(name=i.name, id=i.id)
-
-        post = i.name[0]
-        name = "r" if "rook" in ids else "q" if "queen" in ids else "p" if "pawn" in ids else "n" if "knight" in ids else "k" if "king" in ids else "b" if "bishop" in ids else "t"
-        if i.name[-1] == "w":
-            name = name.upper()
-        print("\"{}\": \"{}\",".format(name + post, ids))
 
 
 add_cogs(bot, COGS)
